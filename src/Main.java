b5c3f4c75946591ef5ec9b3f0d906747a9a891b5
Zoe Aspenns <aspennza@mail.uc.edu>
2024-09-19 12:31:53 -0400

    All tasks complete

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,10 @@ public class Main
         double summerMaintenanceCost = 510.00;
         double fallMaintenanceCost = 205.25;
         double winterMaintenanceCost = 400.25;
+        double totalYearlyMaintenanceCosts = 0.00;
 
+        totalYearlyMaintenanceCosts = springMaintenanceCost + summerMaintenanceCost + fallMaintenanceCost + winterMaintenanceCost;
 
-
-
+        System.out.println("The spring home maintenance cost of $" + springMaintenanceCost + " plus the summer home maintenance cost of $" + summerMaintenanceCost + " plus the fall home maintenance cost of $" + fallMaintenanceCost + " plus the winter home maintenance cost of $" + winterMaintenanceCost + " result in a total yearly home maintenance cost of $" + totalYearlyMaintenanceCosts + ".");
     }
 }
