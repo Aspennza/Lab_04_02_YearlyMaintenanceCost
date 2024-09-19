8579b1653fecd092f41e914acc89d2fdc570b1cf
Zoe Aspenns <aspennza@mail.uc.edu>
2024-09-19 12:09:02 -0400

    Variables entered

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@ public class Main
 {
     public static void main(String[] args)
     {
-        
+        double springMaintenanceCost = 300.50;
+        double summerMaintenanceCost = 510.00;
+        double fallMaintenanceCost = 205.25;
+        double winterMaintenanceCost = 400.25;
+
+
+
+
     }
 }
